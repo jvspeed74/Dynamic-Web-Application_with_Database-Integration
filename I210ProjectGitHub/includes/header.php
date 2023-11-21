726035295758b66284917bea80a461349252c607
Jalen Vaughn <jvspeed74@gmail.com>
2023-11-20 21:53:27 -0500

    Changed filename "I210ProjectLocal" to "I210ProjectGitHub"

--- I210ProjectGitHub/includes/header.php
+++ I210ProjectGitHub/includes/header.php
@@ -8,12 +8,12 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
-    <link type="text/css" rel="stylesheet" href="css/banner.css"/>
-    <link type="text/css" rel="stylesheet" href="css/global.css"/>
-    <link type="text/css" rel="stylesheet" href="css/header.css"/>
-    <link type="text/css" rel="stylesheet" href="css/media.css"/>
-    <link type="text/css" rel="stylesheet" href="css/navitemsandsearch.css"/>
-    <link type="text/css" rel="stylesheet" href="css/product.css"/>
+    <link type="text/css" rel="stylesheet" href="../www/css/banner.css"/>
+    <link type="text/css" rel="stylesheet" href="../www/css/global.css"/>
+    <link type="text/css" rel="stylesheet" href="../www/css/header.css"/>
+    <link type="text/css" rel="stylesheet" href="../www/css/media.css"/>
+    <link type="text/css" rel="stylesheet" href="../www/css/navitemsandsearch.css"/>
+    <link type="text/css" rel="stylesheet" href="../www/css/product.css"/>
     <title><?php echo $pageTitle; ?></title>
 
     <?php
