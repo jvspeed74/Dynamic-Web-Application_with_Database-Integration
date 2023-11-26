9ca908c2ab01f5efab69dd333fe85cb21d66e5ac
jvspeed74 <jvspeed74@gmail.com>
2023-11-26 15:10:34 -0500

    Converted and integrated functionality within project

--- I210ProjectGitHub/removegame.php
+++ I210ProjectGitHub/removegame.php
@@ -7,34 +7,29 @@
  * Description: this script deletes a book from the database.
  *
  */
-$page_title = "Delete Book";
-require_once 'includes/header.php';
-require_once('includes/database.php');
 
-//if book id cannot retrieved, terminate the script.
-if (!filter_has_var(INPUT_GET, 'id')) {
-    $error = "There was a problem retrieving book id.";
-    header("Location: error.php?m=$error");
-    die();
-}
+// Initial Page Requirements
+$pageTitle = "Game Details";
+require('includes/header.php');
 
-//retrieve book id from a query string variable.
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+// Connect to Database
+connect();
 
-//add your code here
-//Define MySQL delete statement
-$sql = "DELETE FROM $tblBook WHERE id=$id";
+// Retrieve and validate game id
+$id = getValidation(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-//execute the query and handle errors
-$query = @$conn->query($sql);
 
-if (!$query) {
-    $error = "Deletion failed: $conn->error.";
-    $conn->close();
-    header("Location: error.php?m=$error");
-    die();
+// Execute query with statement
+/** @var $tableGames */
+runQuery("DELETE FROM $tableGames WHERE id=$id");
+
+
+global $queryData;
+if (!$queryData) {
+    disconnect();
+    raiseError("Deletion failed");
 }
 
 echo "<p>The book has been successfully deleted from the database.</p>";
-$conn->close();
+disconnect();
 require_once 'includes/footer.php';
