e9864f479e10af4183726171fc70213287794b7d
jvspeed74 <jvspeed74@gmail.com>
2023-11-26 15:04:13 -0500

    Converted and integrated functionality within project

--- I210ProjectGitHub/deletegame.php
+++ I210ProjectGitHub/deletegame.php
@@ -1,83 +1,78 @@
 <?php
 /**
- * Author: your name
- * Date: today's date
- * File: deletebook.php
  * Description: This script confirms deletion of book.
  */
-$page_title = "Confirm Book Deletion";
+$pageTitle = "Confirm Game Deletion";
 require_once('includes/header.php');
-require_once('includes/database.php');
 
-//if book id cannot retrieved, terminate the script.
-if (!filter_has_var(INPUT_GET, 'id')) {
-    $error = "There was a problem retrieving book id.";
-    header("Location: error.php?m=$error");
-    die();
-}
+// Connect to Database
+connect();
 
-//retrieve book id from a query string variable.
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+// Retrieve and validate id sent in query string
+$id = getValidation(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-//MySQL SELECT statement
-$sql = "SELECT * FROM $tblBook, $tblCategory WHERE books.category_id = categories.category_id AND id=$id";
+// Execute query with statement
+/** @var $tableGames */
+runQuery("SELECT * FROM $tableGames WHERE id=$id");
 
-//execute the query
-$query = @$conn->query($sql);
-
-//Handle errors
-if (!$query) {
-    $error = "Selection failed: " . $conn->error;
-    $conn->close();
-    header("Location: error.php?m=$error");
-    die();
-}
-
-$row = $query->fetch_assoc();
-if (!$row) {
-    $error = "Book not found";
-    $conn->close();
-    header("Location: error.php?m=$error");
-    die();
-}
+// Get data associated with query
+$rows = fetchData();
 ?>
+    <section>
+        <h2>Game Details</h2>
+        <table>
+            <?php foreach ($rows as $row) { ?>
+                <!-- Display Game image -->
+                <tr>
+                    <td colspan="2"><img src="<?= $row['image'] ?>" alt=""></td>
+                </tr>
 
-    <h2>Book Details</h2>
-    <div class="bookdetails">
-        <div class="cover">
-            <!-- display book image -->
-            <img src="<?= $row['image'] ?>">
-        </div>
-        <div class="label">
-            <!-- display book attributes  -->
-            <div>Title:</div>
-            <div>Author:</div>
-            <div>Category</div>
-            <div>ISBN:</div>
-            <div>Date:</div>
-            <div>Publisher:</div>
-            <div>Price:</div>
-            <div>Description</div>
-        </div>
-
-        <div class="content">
-            <!-- display book details -->
-            <div><?= $row['title'] ?></div>
-            <div><?= $row['author'] ?></div>
-            <div><?= $row['category'] ?></div>
-            <div><?= $row['isbn'] ?></div>
-            <div><?= $row['publish_date'] ?></div>
-            <div><?= $row['publisher'] ?></div>
-            <div>$<?= $row['price'] ?></div>
-            <div><?= $row['description'] ?></div>
-        </div>
-    </div>
-    <div class="bookstore-button">
-        <input type="button" value="Delete"
-               onclick="window.location.href = 'removebook.php?id=<?= $id ?>'">
-        <input type="button" value="Cancel"
-               onclick="window.location.href = 'bookdetails.php?id=<?= $id ?>'">
-        <div style="color: red; display: inline-block;">Are you sure you want to delete the book?</div>
-    </div>
+                <!-- Display Game Attributes -->
+                <tr>
+                    <td>Title:</td>
+                    <td><?= $row['title'] ?></td>
+                </tr>
+                <tr>
+                    <td>Developer:</td>
+                    <td><?= $row['developer'] ?></td>
+                </tr>
+                <tr>
+                    <td>Genre:</td>
+                    <td><?= $row['genre'] ?></td>
+                </tr>
+                <tr>
+                    <td>ESRB:</td>
+                    <td><?= $row['esrb'] ?></td>
+                </tr>
+                <tr>
+                    <td>Release Date:</td>
+                    <td><?= $row['release_date'] ?></td>
+                </tr>
+                <tr>
+                    <td>Publisher:</td>
+                    <td><?= $row['publisher'] ?></td>
+                </tr>
+                <tr>
+                    <td>Price:</td>
+                    <td><?= $row['price'] ?></td>
+                </tr>
+                <tr>
+                    <td>Description:</td>
+                    <td><?= $row['description'] ?></td>
+                </tr>
+            <?php } ?>
+            <tr>
+                <td colspan="2">
+                    <input type="button" onclick="window.location.href='removegame.php?id=<?= $id ?>';"
+                           value="Delete"/>
+                    <input type="button"
+                           onclick="window.location.href='gamedetails.php?id=<?= $id ?>'"
+                           value="Cancel">
+                </td>
+                <td>Are you sure you want to delete this game?
+                </td>
+            </tr>
+        </table>
+    </section>
 <?php
 require_once('includes/footer.php');
