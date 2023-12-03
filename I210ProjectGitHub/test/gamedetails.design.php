fb1c6e78164191a9067f321193a12587b223526f
jvspeed74 <jvspeed74@gmail.com>
2023-12-03 16:06:46 -0500

    Added stuff

--- I210ProjectGitHub/test/gamedetails.design.php
+++ I210ProjectGitHub/test/gamedetails.design.php
@@ -2,7 +2,63 @@
 include ("completed/header.design.php");
 ?>
     <!-- Page Specific Content Starts -->
+<section>
+        <h2>Game Details</h2>
+        <table>
+                <!-- Display Game image -->
+                <tr>
+                    <td colspan="2"><img src="" alt="Image"></td>
+                </tr>
 
+                <!-- Display Game Attributes -->
+                <tr>
+                    <td>Title:</td>
+                    <td>Animal Crossing</td>
+                </tr>
+                <tr>
+                    <td>Developer:</td>
+                    <td>Maha Hada Industries</td>
+                </tr>
+                <tr>
+                    <td>Genre:</td>
+                    <td>Sex Trafficking</td>
+                </tr>
+                <tr>
+                    <td>ESRB:</td>
+                    <td>Mature</td>
+                </tr>
+                <tr>
+                    <td>Release Date:</td>
+                    <td>2024-11-02</td>
+                </tr>
+                <tr>
+                    <td>Publisher:</td>
+                    <td>Ur Mom</td>
+                </tr>
+                <tr>
+                    <td>Price:</td>
+                    <td>$69.99</td>
+                </tr>
+                <tr>
+                    <td>Description:</td>
+                    <td>This game is about random stuff that you probably wouldnt like</td>
+                </tr>
+            <tr>
+                <td colspan="2">
+                        <input type="button" onclick="window.location.href='';"
+                               value="Add to Cart"/>
+                    <input type="button"
+                           onclick="window.location.href=''"
+                           value="Edit">
+                    <input type="button" value="Delete" onclick="window.location.href=''">
+                    <input type="button"
+                           onclick="window.location.href=''"
+                           value="Cancel">
+                </td>
+
+            </tr>
+        </table>
+    </section>
     <!-- Page Specific Content Ends -->
 <?php include ("completed/footer.design.php");
 ?>
