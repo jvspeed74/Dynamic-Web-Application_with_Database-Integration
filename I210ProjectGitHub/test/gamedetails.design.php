65d873991823f6b18d09bb85a439bc897fe70839
jvspeed74 <jvspeed74@gmail.com>
2023-12-03 16:25:16 -0500

    Made compatible

--- I210ProjectGitHub/test/gamedetails.design.php
+++ I210ProjectGitHub/test/gamedetails.design.php
@@ -1,64 +1,100 @@
 <?php
-include ("completed/header.design.php");
+/**
+ * Author: Jalen Vaughn
+ *  Date: 11/16/23
+ * File: gamedetails.php
+ *  Description: This script displays details of a particular game.
+ */
+
+// Initial Page Requirements
+$pageTitle = "Game Details";
+require('completed/header.design.php');
+
+// Connect to Database
+connect();
+
+// Retrieve and validate game id
+$id = "1";
+
+// Execute query with statement
+/** @var $tableGames */
+$query = runQuery("SELECT * FROM $tableGames WHERE id=$id");
+
+// Get data associated with query
+$rows = fetchData($query);
 ?>
-    <!-- Page Specific Content Starts -->
-<section>
+
+    <section>
+        <div class="container">
         <h2>Game Details</h2>
         <table>
+            <?php foreach ($rows as $row) { ?>
                 <!-- Display Game image -->
                 <tr>
-                    <td colspan="2"><img src="" alt="Image"></td>
+                    <td colspan="2"><img src="../www/img/animalcrossingnewhorizons.jpg" alt=""></td>
                 </tr>
 
                 <!-- Display Game Attributes -->
                 <tr>
                     <td>Title:</td>
-                    <td>Animal Crossing</td>
+                    <td><?= $row['title'] ?></td>
                 </tr>
                 <tr>
                     <td>Developer:</td>
-                    <td>Maha Hada Industries</td>
+                    <td><?= $row['developer'] ?></td>
                 </tr>
                 <tr>
                     <td>Genre:</td>
-                    <td>Sex Trafficking</td>
+                    <td><?= $row['genre'] ?></td>
                 </tr>
                 <tr>
                     <td>ESRB:</td>
-                    <td>Mature</td>
+                    <td><?= $row['esrb'] ?></td>
                 </tr>
                 <tr>
                     <td>Release Date:</td>
-                    <td>2024-11-02</td>
+                    <td><?= $row['release_date'] ?></td>
                 </tr>
                 <tr>
                     <td>Publisher:</td>
-                    <td>Ur Mom</td>
+                    <td><?= $row['publisher'] ?></td>
                 </tr>
                 <tr>
                     <td>Price:</td>
-                    <td>$69.99</td>
+                    <td><?= $row['price'] ?></td>
                 </tr>
                 <tr>
                     <td>Description:</td>
-                    <td>This game is about random stuff that you probably wouldnt like</td>
+                    <td><?= $row['description'] ?></td>
                 </tr>
+            <?php } ?>
+            <?php
+            $confirm = "";
+            if (isset($_GET['m'])) {
+                if ($_GET['m'] == "insert") {
+                    $confirm = "You have successfully added the new game.";
+                } else if ($_GET['m'] == "update") {
+                    $confirm = "Your game has been successfully updated.";
+                }
+            }
+            ?>
             <tr>
                 <td colspan="2">
-                        <input type="button" onclick="window.location.href='';"
+                        <input type="button" onclick="window.location.href='includes/shopping/addtocart.inc.php?id=<?= $id ?>';"
                                value="Add to Cart"/>
                     <input type="button"
-                           onclick="window.location.href=''"
+                           onclick="window.location.href='editgame.php?id=<?= $id ?>'"
                            value="Edit">
-                    <input type="button" value="Delete" onclick="window.location.href=''">
+                    <input type="button" value="Delete" onclick="window.location.href='deletegame.php?id=<?= $id ?>'">
                     <input type="button"
-                           onclick="window.location.href=''"
+                           onclick="window.location.href='listgames.php'"
                            value="Cancel">
                 </td>
 
             </tr>
         </table>
+        <div style="color: red; display: inline-block;"><?= $confirm ?></div>
     </section>
-    <!-- Page Specific Content Ends -->
-<?php include ("completed/footer.design.php");
-?>
+<?php
+disconnect();
+include('completed/footer.design.php');
