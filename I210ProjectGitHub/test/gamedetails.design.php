9015e966ced7a53ab3941adf5f24661bc2bd6daf
ayahhin <122550022+ayahhin@users.noreply.github.com>
2023-12-03 15:57:17 -0500

    removed gamedetails.design.php content and moved from inactive

---
+++ I210ProjectGitHub/test/gamedetails.design.php
@@ -0,0 +1,8 @@
+<?php
+include ("completed/header.design.php");
+?>
+    <!-- Page Specific Content Starts -->
+
+    <!-- Page Specific Content Ends -->
+<?php include ("completed/footer.design.php");
+?>
