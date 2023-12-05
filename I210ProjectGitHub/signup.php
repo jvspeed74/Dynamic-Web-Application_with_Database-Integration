40537fd54f5e858d9e733a45d01d2ba399ff20b0
jvspeed74 <jvspeed74@gmail.com>
2023-12-05 12:58:45 -0500

    Fixed a bug where users could create an account with an invalid email

--- I210ProjectGitHub/signup.php
+++ I210ProjectGitHub/signup.php
@@ -11,7 +11,7 @@ checkSignup();
         <form action="includes/authentication/signup.inc.php" method="post">
             <input type="text" name="firstname" maxlength="50" placeholder="Firstname..." required/>
             <input type="text" name="lastname" maxlength="50" placeholder="Lastname..." required/>
-            <input type="text" name="email" maxlength="50" placeholder="Email..." required/>
+            <input type="email" name="email" maxlength="50" placeholder="Email..." required/>
             <input type="text" name="username" maxlength="50" placeholder="Username..." required/>
             <input type="password" name="password" maxlength="255" placeholder="Password..." required/>
             <button type="submit" name="Submit">Sign Up</button>
