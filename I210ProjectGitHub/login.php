f1579a4fd431a6dec2a5e76e95eb67e3cf8e9504
jvspeed74 <jvspeed74@gmail.com>
2023-12-05 12:52:45 -0500

    Deleted obsolete $message variable in login.php

--- I210ProjectGitHub/login.php
+++ I210ProjectGitHub/login.php
@@ -1,14 +1,13 @@
 <?php
 $pageTitle = "Login";
 include("header.php");
-$message = "Please enter your username and password to login.";
 checkLogin();
 ?>
 <!--Page Specific Content Starts-->
 <section>
     <div class="container">
     <h2>Login to your account</h2>
-    <h3><?php echo $message ?></h3>
+    <h3>Please enter your username and password to login.</h3>
     <form action="includes/authentication/login.inc.php" method="post">
         <input type="text" name="username" maxlength="50" placeholder="Username..." required/>
         <input type="password" name="password" maxlength="255" placeholder="Password..." required/>
